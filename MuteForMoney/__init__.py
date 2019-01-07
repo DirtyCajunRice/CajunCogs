246c06fa5bcb0880b2b4823e6d959c78e9881968
Nicholas St. Germain <nick@cajun.pro>
2019-01-07 23:17:12 +0000

    rename

--- MuteForMoney/__init__.py
+++
@@ -1,5 +0,0 @@
-from MuteForMoney.MuteForMoney import MuteForMoney
-
-
-def setup(bot):
-    bot.add_cog(MuteForMoney())
---
+++ muteformoney/__init__.py
@@ -0,0 +1,5 @@
+from muteformoney.MuteForMoney import MuteForMoney
+
+
+def setup(bot):
+    bot.add_cog(MuteForMoney())
